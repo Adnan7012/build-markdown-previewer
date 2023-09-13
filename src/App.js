21028e16de919d854b8f60b43ab55853e459394b
Adnan7012 <wadnan20157012@gmail.com>
2023-09-14 00:33:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import {marked} from "marked"
 import './App.css';
 
+//a header (H1 size), a sub header (H2 size),
+// a link, inline code, a code block, a list item,
+// a blockquote, an image, and bolded text
+
 function App() {
+const [text,setTexst]=useState(`
+ # H1
+
+ ## H2 
+
+ [title](https://www.example.com)
+
+ \`code\`
+
+ \`\`\`
+ {
+   "firstName": "John",
+   "lastName": "Smith",
+   "age": 25
+ }
+ \`\`\`
+
+- First item
+- Second item
+- Third item
+
+> blockquote
+
+![alt text](image.jpg)
+
+**bold text**
+
+`);
+marked.setOptions({
+  breaks:true
+})
+
+const handleChange =(event)=>{
+setTexst(event.target.value)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div id='editorWrap'> 
+        <div id='toolbar'>Editor</div>
+        <textarea id='editor' onChange={handleChange} value={text}></textarea>
+        </div>
+      <div id='previwWrap'> 
+        <div id='toolbar'>Previewer</div>
+        <div id='preview' dangerouslySetInnerHTML={{__html:marked(text)}} ></div>
+      </div>
     </div>
   );
 }
